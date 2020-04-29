7a44ab1eb859b0e52a690dc8036fe56183b10822
Lucas BEE <pouulet@gmail.com>
2020-04-29 19:30:52 +0200

    Use eztv.io API endpoint

--- eztv.go
+++ eztv.go
@@ -4,245 +4,179 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strconv"
+	"strings"
+	"time"
 )
 
-var endpoint = "https://api-fetch.website/tv"
+var endpoint = "https://eztv.io"
+
+const (
+	torrentEndpoint = "/api/get-torrents"
+)
 
 // Eztv errors
 var (
 	ErrEpisodeNotFound = errors.New("episode not found")
 	ErrShowNotFound    = errors.New("show not found")
-	ErrEmptyResponse   = errors.New("empty response from server")
 	ErrInvalidArgument = errors.New("invalid argument")
 	ErrMissingArgument = errors.New("missing argument")
 )
 
-// Status reprensents the status response of a call
-type Status struct {
-	Status     string `json:"status"`
-	Uptime     int64  `json:"uptime"`
-	Server     string `json:"server"`
-	Updated    int64  `json:"updated"`
-	TotalShows int64  `json:"totalShows"`
-	Version    string `json:"version"`
+// response reprensents the response structure of every call
+type response struct {
+	TorrentsCount int               `json:"torrents_count"`
+	Limit         int               `json:"limit"`
+	Page          int               `json:"page"`
+	ImdbID        string            `json:"imdb_id"`
+	Torrents      []*EpisodeTorrent `json:"torrents"`
 }
 
-// Show represents a show
-type Show struct {
-	AirDay        string         `json:"air_day"`
-	AirTime       string         `json:"air_time"`
-	Country       string         `json:"country"`
-	Episodes      []*ShowEpisode `json:"episodes"`
-	Genres        []string       `json:"genres"`
-	ID            string         `json:"_id"`
-	Images        ShowImages     `json:"images"`
-	ImdbID        string         `json:"imdb_id"`
-	LastUpdated   int64          `json:"last_updated"`
-	Network       string         `json:"network"`
-	NumberSeasons int            `json:"num_seasons"`
-	Rating        ShowRating     `json:"rating"`
-	Runtime       string         `json:"runtime"`
-	Slug          string         `json:"slug"`
-	Status        string         `json:"status"`
-	Synopsis      string         `json:"synopsis"`
-	Title         string         `json:"title"`
-	TvdbID        string         `json:"tvdb_id"`
-	Year          string         `json:"year"`
+// EpisodeTorrent represents a torrent for an episode
+type EpisodeTorrent struct {
+	ID              int       `json:"id"`
+	Hash            string    `json:"hash"`
+	Filename        string    `json:"filename"`
+	EpisodeURL      string    `json:"episode_url"`
+	TorrentURL      string    `json:"torrent_url"`
+	MagnetURL       string    `json:"magnet_url"`
+	Title           string    `json:"title"`
+	ImdbID          string    `json:"imdb_id"`
+	Season          int       `json:"season,string"`
+	Episode         int       `json:"episode,string"`
+	SmallScreenshot string    `json:"small_screenshot"`
+	LargeScreenshot string    `json:"large_screenshot"`
+	Seeds           int       `json:"seeds"`
+	Peers           int       `json:"peers"`
+	DateReleased    time.Time `json:"date_released_unix"`
+	Size            uint      `json:"size_bytes,string"`
 }
 
-// ShowEpisode represents a show episode
-type ShowEpisode struct {
-	Episode    int                     `json:"episode"`
-	FirstAired int                     `json:"first_aired"`
-	Overview   string                  `json:"overview"`
-	Season     int                     `json:"season"`
-	Title      string                  `json:"title"`
-	TvdbID     int                     `json:"tvdb_id"`
-	Torrents   map[string]*ShowTorrent `json:"torrents"`
-}
-
-// ShowImages represents the show images
-type ShowImages struct {
-	Poster string `json:"poster"`
-	Fanart string `json:"fanart"`
-	Banner string `json:"banner"`
-}
-
-// ShowRating respresents the show ratings
-type ShowRating struct {
-	Percentage float64 `json:"percentage"`
-	Votes      int     `json:"votes"`
-	Loved      int     `json:"loved"`
-	Hated      int     `json:"hated"`
-}
-
-// ShowSeason represents a show season
-type ShowSeason struct {
-	Episodes map[string]*ShowEpisode
-}
-
-// ShowTorrent represents the show torrent
-type ShowTorrent struct {
-	Peers int    `json:"peers"`
-	Seeds int    `json:"seeds"`
-	URL   string `json:"url"`
-}
-
-// Ping will make a simple GET on the root path of the api to see if it's alive
-func Ping() (*Status, error) {
-	// Generate URL
-	u, err := url.Parse(endpoint)
+// UnmarshalJSON implements the Unmarshaller interface
+func (t *EpisodeTorrent) UnmarshalJSON(data []byte) error {
+	type Alias EpisodeTorrent
+	// Unmarshal everything except the fields we need to edit
+	aux := &struct {
+		*Alias
+		ImdbID          string `json:"imdb_id"`
+		DateReleased    int64  `json:"date_released_unix"`
+		SmallScreenshot string `json:"small_screenshot"`
+		LargeScreenshot string `json:"large_screenshot"`
+	}{}
+	err := json.Unmarshal(data, aux)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
+	// Copy the whole thing
+	*t = EpisodeTorrent(*(aux.Alias))
+	// Set the proper DateReleased time / imdbID / screenshots URLs
+	t.DateReleased = time.Unix(aux.DateReleased, 0)
+	t.ImdbID = "tt" + aux.ImdbID
+	t.SmallScreenshot = "https:" + aux.SmallScreenshot
+	t.LargeScreenshot = "https:" + aux.LargeScreenshot
+
+	return nil
+}
+
+func get(path string, v *url.Values) (*response, error) {
 	// Make the request
-	resp, err := http.Get(u.String())
+	resp, err := http.Get(endpoint + path + "?" + v.Encode())
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 
-	// Parse the response
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("got status %d when making the request", resp.StatusCode)
 	}
 
-	status := &Status{}
-	err = json.Unmarshal(body, status)
-	if err != nil {
-		return nil, err
-	}
-	return status, nil
+	r := &response{}
+	return r, json.NewDecoder(resp.Body).Decode(&r)
 }
 
-// GetShowDetails will get the infos about a show from an ImdbID
-func GetShowDetails(ImdbID string) (*Show, error) {
-	if ImdbID == "" {
+// getAll makes requests on all the pages until we have all the torrents
+func getAll(path string, v *url.Values) ([]*EpisodeTorrent, error) {
+	var maxPages = 20
+
+	var torrents []*EpisodeTorrent
+	for i := 1; i < maxPages; i++ {
+		v.Add("page", strconv.Itoa(i))
+		v.Add("limit", "100")
+		resp, err := get(path, v)
+		if err != nil {
+			return nil, err
+		}
+		// No ImdbID or no torrents are considered as a failure
+		if resp.ImdbID == "" || resp.TorrentsCount == 0 {
+			return nil, ErrShowNotFound
+		}
+
+		torrents = append(torrents, resp.Torrents...)
+		// We asked for a 100 torrents, so if we got less, then the search is
+		// over
+		if len(resp.Torrents) < 100 {
+			break
+		}
+	}
+	return torrents, nil
+}
+
+// GetShowTorrents will get the torrents of a show from an ImdbID
+func GetShowTorrents(imdbID string) ([]*EpisodeTorrent, error) {
+	if imdbID == "" {
 		return nil, ErrMissingArgument
 	}
-	// Generate URL
-	u, err := url.Parse(fmt.Sprintf("%s/show/%s", endpoint, ImdbID))
-	if err != nil {
-		return nil, err
-	}
+	// The API only accepts imdbIDs without their "tt"
+	imdbID = strings.Replace(imdbID, "t", "", -1)
+
+	v := &url.Values{}
+	v.Add("imdb_id", imdbID)
 
 	// Make the request
-	resp, err := http.Get(u.String())
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	// Parse the response
-	body, err := ioutil.ReadAll(resp.Body)
+	torrents, err := getAll(torrentEndpoint, v)
 	if err != nil {
 		return nil, err
 	}
 
-	// Test for an empty body, a bit hacky but the API returns an empty
-	// response sometimes...
-	// 2 is for "{}" which is the smallest JSON response
-	if len(body) < 2 {
-		return nil, ErrEmptyResponse
-	}
-
-	s := &Show{}
-	err = json.Unmarshal(body, s)
-	if err != nil {
-		return nil, err
-	}
-
-	// No title is considered as a failure
-	if s.Title == "" {
-		return nil, ErrShowNotFound
-	}
-
-	return s, nil
+	return torrents, nil
 }
 
-// GetEpisode will get the infos about a show's episode from an ImdbID,
+// GetEpisodeTorrents will get the torrents of show's episode from an ImdbID,
 // a season number and an episode number
-func GetEpisode(ImdbID string, season, episode int) (*ShowEpisode, error) {
-	s, err := GetShowDetails(ImdbID)
+func GetEpisodeTorrents(ImdbID string, season, episode int) ([]*EpisodeTorrent, error) {
+	episodeTorrents := []*EpisodeTorrent{}
+	torrents, err := GetShowTorrents(ImdbID)
 	if err != nil {
 		return nil, err
 	}
-	for _, e := range s.Episodes {
+
+	for _, e := range torrents {
 		if e.Episode == episode && e.Season == season {
-			return e, nil
+			episodeTorrents = append(episodeTorrents, e)
 		}
 	}
 
-	return nil, ErrEpisodeNotFound
+	if len(episodeTorrents) == 0 {
+		return nil, ErrEpisodeNotFound
+	}
+	return episodeTorrents, nil
 }
 
-// GetSeason will get the infos about all the show's episode from an
-// ImdbID and a season number
-func GetSeason(ImdbID string, season int) ([]*ShowEpisode, error) {
-	s, err := GetShowDetails(ImdbID)
-	if err != nil {
-		return nil, err
-	}
-	episodes := []*ShowEpisode{}
-	for _, e := range s.Episodes {
-		if e.Season == season {
-			episodes = append(episodes, e)
-		}
-	}
-
-	return episodes, nil
-}
-
-// ListShows returns a list of 50 shows with metadata
-func ListShows(page int) ([]*Show, error) {
-	return getShows("", page)
-}
-
-// SearchShow will get the infos about a show from a string
-func SearchShow(keyword string) ([]*Show, error) {
-	if keyword == "" {
-		return nil, ErrMissingArgument
-	}
-	return getShows(keyword, 1)
-}
-
-func getShows(keyword string, page int) ([]*Show, error) {
-	if page <= 0 {
-		return nil, ErrInvalidArgument
-	}
+// GetTorrents will get torrents, given a limit and a page number
+func GetTorrents(limit, page int) ([]*EpisodeTorrent, error) {
 	// Generate URL
-	u, err := url.Parse(fmt.Sprintf("%s/shows/%d", endpoint, page))
-	if err != nil {
-		return nil, err
-	}
-	urlValues := &url.Values{}
-	if keyword != "" {
-		urlValues.Add("keywords", keyword)
-	}
-	u.RawQuery = urlValues.Encode()
+	v := &url.Values{}
+	v.Add("limit", strconv.Itoa(limit))
+	v.Add("page", strconv.Itoa(page))
 
 	// Make the request
-	resp, err := http.Get(u.String())
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	// Parse the response
-	body, err := ioutil.ReadAll(resp.Body)
+	r, err := get(torrentEndpoint, v)
 	if err != nil {
 		return nil, err
 	}
 
-	s := []*Show{}
-	err = json.Unmarshal(body, &s)
-	if err != nil {
-		return nil, err
-	}
-	return s, nil
+	return r.Torrents, nil
 }
--- eztv_test.go
+++ eztv_test.go
@@ -6,260 +6,154 @@ import (
 	"net/http/httptest"
 	"reflect"
 	"testing"
+	"time"
 )
 
-// TestPing tests the Ping method
-func TestPing(t *testing.T) {
-	rawHTMLResponse := `{"status":"online","uptime":2381142,"server":"serv01","totalShows":1426,"updated":1454655986,"version":"1.0.3"}`
-
+// TestShowNotFound tests the ErrShowNotFound error
+func TestShowNotFound(t *testing.T) {
+	var requestURI string
+	rawHTMLResponse := `{ "torrents_count": 280450, "limit": 30, "page": 1, "torrents": [] }`
 	// Fake server with a fake answer
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		requestURI = r.RequestURI
 		fmt.Fprintln(w, rawHTMLResponse)
 	}))
 	defer ts.Close()
 	endpoint = ts.URL
 
-	status, err := Ping()
-	if err != nil {
-		t.Errorf("Error when pinging the endpoint")
-	}
-
-	// Expected result
-	expectedResult := &Status{
-		Status:     "online",
-		Uptime:     2381142,
-		Server:     "serv01",
-		TotalShows: 1426,
-		Updated:    1454655986,
-		Version:    "1.0.3",
-	}
-
-	if reflect.DeepEqual(status, expectedResult) == false {
-		t.Errorf("Response not properly set")
-	}
-}
-
-// TestShowNotFoundEmptyResponse tests the ErrShowNotFound error
-func TestShowNotFoundEmptyResponse(t *testing.T) {
-	// Fake server with a fake answer
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "")
-	}))
-	defer ts.Close()
-	endpoint = ts.URL
-
-	_, err := GetShowDetails("fakeID")
+	_, err := GetShowTorrents("fakeID")
 	if err == nil {
-		t.Fatalf("Got no error, expected %q", ErrEmptyResponse)
+		t.Fatalf("got no error, expected %q", ErrShowNotFound)
 	}
-
-	if err != ErrEmptyResponse {
-		t.Fatalf("Expected %q, got %q", ErrEmptyResponse, err)
-	}
-}
-
-// TestShowNotFound tests the ErrShowNotFound error
-func TestShowNotFound(t *testing.T) {
-	// Fake server with a fake answer
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "{}")
-	}))
-	defer ts.Close()
-	endpoint = ts.URL
-
-	_, err := GetShowDetails("fakeID")
-	if err == nil {
-		t.Fatalf("Got no error, expected %q", ErrShowNotFound)
+	expectedRequestURI := "/api/get-torrents?imdb_id=fakeID&limit=100&page=1"
+	if requestURI != expectedRequestURI {
+		t.Fatalf("expected URL %q, got %q", expectedRequestURI, requestURI)
 	}
 
 	if err != ErrShowNotFound {
-		t.Fatalf("Expected %q, got %q", ErrShowNotFound, err)
+		t.Fatalf("expected %q, got %q", ErrShowNotFound, err)
 	}
 }
 
-// TestBadGetShowDetails tests GetShowDetails with missing arguments
+// TestBadGetShowDetails tests GetShowTorrents with missing arguments
 func TestBadGetShowDetails(t *testing.T) {
-	_, err := GetShowDetails("")
+	_, err := GetShowTorrents("")
 	if err == nil {
-		t.Errorf("Should raise an error with missing argument")
+		t.Fatalf("should raise an error with missing argument")
 	}
 	if err != ErrMissingArgument {
-		t.Errorf("Should raise an error ErrMissingArgument")
+		t.Errorf("should raise an error ErrMissingArgument")
 	}
 }
 
-// TestGetShowDetails tests GetShowDetails with missing arguments
-func TestGetShowDetails(t *testing.T) {
+// TestGetShowTorrents tests GetShowTorrents
+func TestGetShowTorrents(t *testing.T) {
 	// Fake server with a fake answer
+	var requestURI string
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, rawHTMLResponseGetShowDetails)
+		requestURI = r.RequestURI
+		fmt.Fprintln(w, rawResponseGetTorrents)
 	}))
 	defer ts.Close()
 	endpoint = ts.URL
 
-	s, err := GetShowDetails("tt2085059")
+	s, err := GetShowTorrents("tt0383795")
 	if err != nil {
-		t.Errorf("Error while getting show details %+v", err)
+		t.Fatalf("error while getting show torrents %+v", err)
 	}
 
-	expectedResult := &Show{
-		AirDay:        "Tuesday",
-		AirTime:       "21:00",
-		Country:       "gb",
-		ID:            "tt2085059",
-		ImdbID:        "tt2085059",
-		LastUpdated:   1430654860060,
-		Network:       "Channel 4",
-		NumberSeasons: 2,
-		Runtime:       "60",
-		Slug:          "black-mirror",
-		Status:        "returning series",
-		Synopsis:      "Over the last ten years, ...",
-		Title:         "Black Mirror",
-		TvdbID:        "253463",
-		Year:          "2011",
-		Genres:        []string{"Drama"},
-		Rating: ShowRating{
-			Percentage: 87.1234,
-			Votes:      2662,
-			Loved:      0,
-			Hated:      0,
-		},
-		Images: ShowImages{
-			Poster: "https://walter.trakt.us/images/shows/000/041/793/posters/original/bc21969723.jpg",
-			Fanart: "https://walter.trakt.us/images/shows/000/041/793/fanarts/original/546a01eb67.jpg",
-			Banner: "placeholders/banner.png",
-		},
-		Episodes: []*ShowEpisode{
-			expectedEpisode,
-		},
+	expectedRequestURI := "/api/get-torrents?imdb_id=0383795&limit=100&page=1"
+	if requestURI != expectedRequestURI {
+		t.Fatalf("expected URL %q, got %q", expectedRequestURI, requestURI)
 	}
-	if reflect.DeepEqual(s, expectedResult) == false {
-		t.Errorf("Response not properly set\n Expecting %+v\nAnd got %+v", expectedResult, s)
+
+	if reflect.DeepEqual(s, expectedTorrents) == false {
+		t.Errorf("invalid torrent, expecting:\n %+v\nand got:\n%+v", expectedTorrents[0], s[0])
 	}
 }
 
-func TestGetEpisode(t *testing.T) {
+// TestGetEpisodeTorrents tests GetEpisodeTorrents
+func TestGetEpisodeTorrents(t *testing.T) {
 	// Fake server with a fake answer
+	var requestURI string
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, rawHTMLResponseGetShowDetails)
+		requestURI = r.RequestURI
+		fmt.Fprintln(w, rawResponseGetTorrents)
 	}))
 	defer ts.Close()
 	endpoint = ts.URL
 
-	e, err := GetEpisode("tt2085059", 1, 1)
+	e, err := GetEpisodeTorrents("tt2085059", 1, 1)
 	if err == nil {
-		t.Errorf("No errors while getting unknown show details %+v", err)
+		t.Errorf("no errors while getting unknown show details %+v", err)
 	}
 	if e != nil {
-		t.Error("Episode should be nil", e)
+		t.Error("episode should be nil", e)
 	}
 	if err != ErrEpisodeNotFound {
-		t.Error("Error should Be ErrEpisodeNotFound", err)
+		t.Error("error should be ErrEpisodeNotFound", err)
 	}
 
-	e, err = GetEpisode("tt2085059", 2, 1)
+	e, err = GetEpisodeTorrents("tt0383795", 1, 10)
 	if err != nil {
-		t.Errorf("Error while getting show details %+v", err)
+		t.Errorf("error while getting episode torrents %+v", err)
 	}
-	if reflect.DeepEqual(e, expectedEpisode) == false {
-		t.Errorf("Response not properly set\n Expecting %+v\nAnd got %+v", expectedEpisode, e)
+
+	expectedRequestURI := "/api/get-torrents?imdb_id=0383795&limit=100&page=1"
+	if requestURI != expectedRequestURI {
+		t.Fatalf("expected URL %q, got %q", expectedRequestURI, requestURI)
+	}
+
+	if reflect.DeepEqual(e, expectedTorrents) == false {
+		t.Errorf("invalid torrent, expecting:\n%+v\nand got:\n%+v", expectedTorrents, e)
 	}
 }
 
-// TestGetSeason will test the function to get all the episodes of a season
-func TestGetSeason(t *testing.T) {
+// TestGetTorrents tests GetTorrents
+func TestGetTorrents(t *testing.T) {
 	// Fake server with a fake answer
+	var requestURI string
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, rawHTMLResponseGetShowDetails)
+		requestURI = r.RequestURI
+		fmt.Fprintln(w, rawResponseGetTorrents)
 	}))
 	defer ts.Close()
 	endpoint = ts.URL
 
-	// With a non-existing season, we should have an empty list
-	emptyList, err := GetSeason("tt2085059", 5)
+	e, err := GetTorrents(10, 1)
 	if err != nil {
-		t.Errorf("Error while getting show details %+v", err)
-	}
-	expectedEmptyResult := []*ShowEpisode{}
-	if reflect.DeepEqual(emptyList, expectedEmptyResult) == false {
-		t.Errorf("Response not properly set\n Expecting %+v\nAnd got %+v", expectedEmptyResult, emptyList)
+		t.Errorf("error while getting torrents %+v", err)
 	}
 
-	// With an existing season, we should have a list of episodes
-	e, err := GetSeason("tt2085059", 2)
-	if err != nil {
-		t.Errorf("Error while getting show details %+v", err)
+	expectedRequestURI := "/api/get-torrents?limit=10&page=1"
+	if requestURI != expectedRequestURI {
+		t.Fatalf("expected URL %q, got %q", expectedRequestURI, requestURI)
 	}
-	expectedResult := []*ShowEpisode{
-		expectedEpisode,
-	}
-	if reflect.DeepEqual(e, expectedResult) == false {
-		t.Errorf("Response not properly set\n Expecting %+v\nAnd got %+v", expectedResult, e)
+
+	if reflect.DeepEqual(e, expectedTorrents) == false {
+		t.Errorf("invalid torrent, expecting:\n%+v\nand got:\n%+v", expectedTorrents, e)
 	}
 }
 
-// TestSearchShow will test the search of a show
-func TestSearchShow(t *testing.T) {
-	// Fake server with a fake answer
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, rawHTMLResponseSearchShow)
-	}))
-	defer ts.Close()
-	endpoint = ts.URL
-	s, err := SearchShow("black mirror")
-	if err != nil {
-		t.Errorf("Error while searching show %+v", err)
-	}
-	expectedResult := []*Show{
-		&Show{
-			ID:            "tt2085059",
-			ImdbID:        "tt2085059",
-			LastUpdated:   1430654860060,
-			NumberSeasons: 2,
-			Slug:          "black-mirror",
-			Title:         "Black Mirror",
-			TvdbID:        "253463",
-			Year:          "2011",
-			Rating: ShowRating{
-				Percentage: 87,
-				Votes:      2662,
-				Loved:      0,
-				Hated:      0,
-			},
-			Images: ShowImages{
-				Poster: "https://walter.trakt.us/images/shows/000/041/793/posters/original/bc21969723.jpg",
-				Fanart: "https://walter.trakt.us/images/shows/000/041/793/fanarts/original/546a01eb67.jpg",
-				Banner: "placeholders/banner.png",
-			},
-		},
-	}
-	if reflect.DeepEqual(s, expectedResult) == false {
-		t.Errorf("Response not properly set\n Expecting %+v\nAnd got %+v", expectedResult, s)
-	}
-}
+var rawResponseGetTorrents = `{ "torrents_count": 280450, "limit": 30, "page": 1, "imdb_id": "0383795", "torrents": [{ "id": 1443504, "hash": "033712dc2a9c6b23edbcbea737ee6ecf02619c2e", "filename": "The.Joy.of.Painting.S01E10.INTERNAL.480p.x264-mSD[eztv].mkv", "episode_url": "https://eztv.io/ep/1443504/the-joy-of-painting-s01e10-internal-480p-x264-msd/", "torrent_url": "https://zoink.ch/torrent/The.Joy.of.Painting.S01E10.INTERNAL.480p.x264-mSD[eztv].mkv.torrent", "magnet_url": "magnet:?xt=urn:btih:033712dc2a9c6b23edbcbea737ee6ecf02619c2e&dn=The.Joy.of.Painting.S01E10.INTERNAL.480p.x264-mSD%5Beztv%5D&tr=udp://tracker.coppersurfer.tk:80&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://exodus.desync.com:6969", "title": "The Joy of Painting S01E10 INTERNAL 480p x264-mSD EZTV", "imdb_id": "0383795", "season": "1", "episode": "10", "small_screenshot": "//ezimg.ch/thumbs/the-joy-of-painting-s01e10-internal-480p-x264-msd-small.jpg", "large_screenshot": "//ezimg.ch/thumbs/the-joy-of-painting-s01e10-internal-480p-x264-msd-large.jpg", "seeds": 64, "peers": 0,"date_released_unix": 1588784249, "size_bytes": "86643580"}]}`
 
-var rawHTMLResponseSearchShow = `[{"_id":"tt2085059","images":{"poster":"https://walter.trakt.us/images/shows/000/041/793/posters/original/bc21969723.jpg","fanart":"https://walter.trakt.us/images/shows/000/041/793/fanarts/original/546a01eb67.jpg","banner":"placeholders/banner.png"},"imdb_id":"tt2085059","last_updated":1430654860060,"num_seasons":2,"rating":{"percentage":87,"votes":2662,"loved":0,"hated":0},"slug":"black-mirror","title":"Black Mirror","tvdb_id":"253463","year":"2011"}]`
-
-var rawHTMLResponseGetShowDetails = `{"_id": "tt2085059","air_day": "Tuesday","air_time": "21:00","country": "gb","imdb_id": "tt2085059","last_updated": 1430654860060,"network": "Channel 4","num_seasons": 2,"rating": {"hated": 0,"loved": 0,"percentage": 87.1234,"votes": 2662},"runtime": "60","slug": "black-mirror","status": "returning series","synopsis": "Over the last ten years, ...","title": "Black Mirror","tvdb_id": "253463","year": "2011","episodes": [{"date_based": false,"episode": 1,"first_aired": 1360616400,"overview": "Martha and Ash are a young couple ....","season": 2,"title": "Be Right Back","torrents": {"480p": {"peers": 0,"seeds": 0,"url": "magnet:?xt=urn:btih:Z4I3PBZVO&dn=Black.Mirror.2x01&tr=udp://tracker.openent.com:80/"},"720p": {"peers": 0,"seeds": 0,"url": "magnet:?xt=urn:btih:LSVN23DMU&dn=Black.Mirror.2x01tr=udp://tracker.openent.com:80/"}},"tvdb_id": 4418485,"watched": {"watched": false}}],"genres": ["Drama"],"images": {"banner": "placeholders/banner.png","fanart": "https://walter.trakt.us/images/shows/000/041/793/fanarts/original/546a01eb67.jpg","poster": "https://walter.trakt.us/images/shows/000/041/793/posters/original/bc21969723.jpg"}}`
-
-var expectedEpisode = &ShowEpisode{
-	Episode:    1,
-	FirstAired: 1360616400,
-	Overview:   "Martha and Ash are a young couple ....",
-	Season:     2,
-	Title:      "Be Right Back",
-	TvdbID:     4418485,
-	Torrents: map[string]*ShowTorrent{
-		"480p": &ShowTorrent{
-			Peers: 0,
-			Seeds: 0,
-			URL:   "magnet:?xt=urn:btih:Z4I3PBZVO&dn=Black.Mirror.2x01&tr=udp://tracker.openent.com:80/",
-		},
-		"720p": &ShowTorrent{
-			Peers: 0,
-			Seeds: 0,
-			URL:   "magnet:?xt=urn:btih:LSVN23DMU&dn=Black.Mirror.2x01tr=udp://tracker.openent.com:80/",
-		},
+var expectedTorrents = []*EpisodeTorrent{
+	&EpisodeTorrent{
+		ID:              1443504,
+		Hash:            "033712dc2a9c6b23edbcbea737ee6ecf02619c2e",
+		Filename:        "The.Joy.of.Painting.S01E10.INTERNAL.480p.x264-mSD[eztv].mkv",
+		EpisodeURL:      "https://eztv.io/ep/1443504/the-joy-of-painting-s01e10-internal-480p-x264-msd/",
+		TorrentURL:      "https://zoink.ch/torrent/The.Joy.of.Painting.S01E10.INTERNAL.480p.x264-mSD[eztv].mkv.torrent",
+		MagnetURL:       "magnet:?xt=urn:btih:033712dc2a9c6b23edbcbea737ee6ecf02619c2e&dn=The.Joy.of.Painting.S01E10.INTERNAL.480p.x264-mSD%5Beztv%5D&tr=udp://tracker.coppersurfer.tk:80&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://exodus.desync.com:6969",
+		Title:           "The Joy of Painting S01E10 INTERNAL 480p x264-mSD EZTV",
+		ImdbID:          "tt0383795",
+		Season:          1,
+		Episode:         10,
+		SmallScreenshot: "https://ezimg.ch/thumbs/the-joy-of-painting-s01e10-internal-480p-x264-msd-small.jpg",
+		LargeScreenshot: "https://ezimg.ch/thumbs/the-joy-of-painting-s01e10-internal-480p-x264-msd-large.jpg",
+		Seeds:           64,
+		Peers:           0,
+		DateReleased:    time.Unix(1588784249, 0),
+		Size:            86643580,
 	},
 }
